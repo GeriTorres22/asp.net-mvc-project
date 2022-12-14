7cc0507776c9f9ff36be359add549f070d6eeb83
GeriTorres22 <120600140+GeriTorres22@users.noreply.github.com>
2022-12-14 14:28:26 -0600

    Add files via upload
    adding full project files

---
+++ DAL/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieApp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MovieApp.DAL
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext() : base("MovieContext")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres{ get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ DAL/MovieInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MovieApp.Models;
+
+namespace MovieApp.DAL
+{
+    public class MovieInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<MovieContext>
+    {
+        protected override void Seed(MovieContext context)
+        {
+            new Genre { Name = "Comedy" };
+            context.SaveChanges();
+
+
+        }
+    }
+}
